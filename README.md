f49d0b8d40d584ee6f551bec3de60bb5b8fb4ca0
lutherannn <michael.eskelin@gmaiil.com>
2022-12-14 18:56:31 -0500

    added lua, that was easier than expected

--- README.md
+++ README.md
@@ -2,3 +2,7 @@
 
 Making tic-tac-toe games in different languages<br>
 My liver really starting to slack, and i can feel myself decaying from the cigarette packs
+
+# TODO
+
+My win checking is very bad lol, i'll update it for the ones i have finished soon
---
+++ lua/ttt.lua
@@ -0,0 +1,147 @@
+-- why does lua indexing start at 1? who knows but it makes this less confusing at least.
+local board = {"*", "*", "*", "*", "*", "*", "*", "*", "*"}
+local legalSquares = {}
+local validChoice = false
+
+local function clear()
+    local ok, err, code = os.rename("C:\\Windows", "C:\\Windows")
+    if not ok then
+        if code == 13 then
+            os.execute("cls")
+        end
+    else
+        os.execute("clear")
+    end
+    os.execute("cls")
+end
+
+local function printBoard()
+    newlCounter = 0
+    for i = 1, #board do
+        io.write(board[i])
+        newlCounter = newlCounter + 1
+
+        if newlCounter == 3 then
+            io.write("\n")
+            newlCounter = 0
+        end
+    end
+end
+
+local function getIndexes()
+    for i = 1, #board do
+        if board[i] == "*" then
+            table.insert(legalSquares, i)
+        end
+    end
+end
+
+local function userTurn()
+    while validChoice == false do
+        io.write("Choose a square to put your X: ")
+        userChoice = io.read("*n")
+
+        if board[userChoice] == "*" then
+            board[userChoice] = "X"
+            validChoice = true
+        else
+            print("Square not valid or already taken.")
+        end
+    end
+    validChoice = false
+end
+
+local function cpuTurn()
+    while validChoice == false do
+        cpuChoice = math.random(#board)
+        if board[cpuChoice] == "*" then
+            board[cpuChoice] = "O"
+            validChoice = true
+        end
+    end
+    validChoice = false
+end
+
+local function checkWin()
+    if board[1] == "X" and board[2] == "X" and board[3] == "X" then
+        return "u"
+    end
+    if board[4] == "X" and board[5] == "X" and board[6] == "X" then
+        return "u"
+    end
+    if board[7] == "X" and board[8] == "X" and board[9] == "X" then
+        return "u"
+    end
+    if board[1] == "X" and board[4] == "X" and board[7] == "X" then
+        return "u"
+    end
+    if board[2] == "X" and board[5] == "X" and board[8] == "X" then
+        return "u"
+    end
+    if board[3] == "X" and board[6] == "X" and board[9] == "X" then
+        return "u"
+    end
+    if board[1] == "X" and board[5] == "X" and board[9] == "X" then
+        return "u"
+    end
+    if board[3] == "X" and board[5] == "X" and board[7] == "X" then
+        return "u"
+    end
+
+    if board[1] == "O" and board[2] == "O" and board[3] == "O" then
+        return "c"
+    end
+    if board[4] == "O" and board[5] == "O" and board[6] == "O" then
+        return "c"
+    end
+    if board[7] == "O" and board[8] == "O" and board[9] == "O" then
+        return "c"
+    end
+    if board[1] == "O" and board[4] == "O" and board[7] == "O" then
+        return "c"
+    end
+    if board[2] == "O" and board[5] == "O" and board[8] == "O" then
+        return "c"
+    end
+    if board[3] == "O" and board[6] == "O" and board[9] == "O" then
+        return "c"
+    end
+    if board[1] == "O" and board[5] == "O" and board[9] == "O" then
+        return "c"
+    end
+    if board[3] == "O" and board[5] == "O" and board[7] == "O" then
+        return "c"
+    end
+
+    for i = 1, #board do
+        if board[i] ~= "*" then
+            break
+        end
+    end
+    return "d"
+end
+
+while true do
+    clear()
+    printBoard()
+    userTurn()
+    clear()
+    printBoard()
+    if checkWin() == "u" then
+        print("You win!")
+        os.exit()
+    end
+    if checkWin() == "d" then
+        print("Tie!")
+    end
+    cpuTurn()
+    clear()
+    printBoard()
+    if checkWin() == "c" then
+        print("You win!")
+        os.exit()
+    end
+    if checkWin() == "d" then
+        print("Tie!")
+    end
+end
